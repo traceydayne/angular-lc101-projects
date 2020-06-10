42913a624603d6966fd86866b9d0a852fcee1934
Tracey Chandler <tracey.culhane@gmail.com>
2020-06-10 14:18:53 -0500

    first draft

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My favorite pictures!';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://image.shutterstock.com/image-photo/bernese-mountain-puppy-basket-outdoors-600w-558354661.jpg';
+  image3 = 'https://image.shutterstock.com/image-photo/kitten-grass-sun-260nw-486546004.jpg';
 
   constructor() { }
 
